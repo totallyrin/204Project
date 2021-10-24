d763b0d079dfe208ed916f414ec696f65282d730
totallyrin <lucy.woloszczuk@gmail.com>
2021-10-24 16:50:58 -0400

    corrected formatting of properties file

--- properties.py
+++ properties.py
@@ -3,6 +3,7 @@
 # num of seats
 from pprint import pprint
 
+
 def add_element(dic, key, val):
     if key in dic:
         dic[key].append(val)
@@ -10,20 +11,20 @@ def add_element(dic, key, val):
         dic[key] = [val]
     return dic
 
-def convert_properties():
 
+def convert_properties():
     seats = {}
-    rating = {} # rating
-    price = {} # price ($, $$, $$$)
-    dietary = {} # dietary considerations
-    fastFood = {} # 'fast-food' or not
-    seating = {} # indoor/outdoor seating
-    parking = {} # vehicular parking, bike rack
-    service = {} # take-out, eat in, delivery
-    hours = {} # open morning, afternoon, evenings, nights
+    rating = {}  # rating
+    price = {}  # price ($, $$, $$$)
+    dietary = {}  # dietary considerations
+    fastFood = {}  # 'fast-food' or not
+    seating = {}  # indoor/outdoor seating
+    parking = {}  # vehicular parking, bike rack
+    service = {}  # take-out, eat in, delivery
+    hours = {}  # open morning, afternoon, evenings, nights
 
     properties = [seats, rating, price, dietary, fastFood, seating, parking, service, hours]
-    
+
     for i in restaurants:
         for j in range(len(properties)):
             if type(i[j + 1]) is list:
@@ -33,6 +34,7 @@ def convert_properties():
                 add_element(properties[j], i[j + 1], i[0])
     return properties
 
+
 restaurants = [
     ["McDonald's", 2, 3, "$", ["veg", "gluten"], True, ["indoor"], ["bike"], ["take-out", "eat-in", "delivery"], [6,
                                                                                                                   22]],
@@ -41,7 +43,6 @@ restaurants = [
     ["Tommy's", 4, 4, "$", ["gluten"], True, ["indoor", "outdoor"], ["bike", "vehicle"], ["take-out", "eat-in",
                                                                                           "delivery"], [8, 2]]
 ]
-
 
 if __name__ == "__main__":
     pprint(convert_properties())
